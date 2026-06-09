61ef9cc0e7c99dce0777c29b88251da9173b0c04
farisabujolban <faris.abujolban@gmail.com>
2026-06-09 08:32:53 -0700

    ci: upload coverage to codecov

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
         include: ['tests/**/*.test.ts'],
         coverage: {
             provider: 'v8',
+            reporter: ['text', 'lcov'],
             include: ['src/**/*.ts'],
             thresholds: {
                 lines: 80,
